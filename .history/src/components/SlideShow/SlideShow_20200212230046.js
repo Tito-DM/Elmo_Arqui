8694d200b255fe6837e3435df7342d82d26ae818
pyong <muanda1993@hotmail.com>
2020-02-13 01:38:43 +0000

    complete slide style

---
+++ .history/src/components/SlideShow/SlideShow_20200212230046.js
@@ -0,0 +1,29 @@
+import React, { Component, useEffect, useState } from "react";
+import M from "materialize-css/dist/js/materialize.min.js";
+import slideImg1 from "../../asset/img/slideImg.jpg";
+
+const SlideShow = () => {
+	useEffect(() => {
+		var elems = document.querySelectorAll(".carousel.carousel-slider");
+		M.Carousel.init(elems, { fullWidth: true, indicators: true, duration: 10 });
+
+		setInterval(() => {
+			M.Carousel.init(elems, "next");
+		}, 100);
+	}, []);
+
+	const [SlideShow, SetSlideShow] = useState({
+		img: slideImg1,
+		title: "Projectando um Futuro Melhor e saudavel",
+		subTitle: "Para Ti e a Sua Familia"
+	});
+
+	return (
+    <div className="slider">
+
+    </div>
+
+	);
+};
+
+export default SlideShow;
