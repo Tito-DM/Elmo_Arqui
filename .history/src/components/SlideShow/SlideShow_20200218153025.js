ea3638a5791a822c58a868a4f0824a8a06ce27a1
pyong <muanda1993@hotmail.com>
2020-02-18 15:46:43 +0000

    clean up warnning and erros

---
+++ .history/src/components/SlideShow/SlideShow_20200218153025.js
@@ -0,0 +1,71 @@
+import React, {  useEffect, useState, Fragment } from "react";
+import "./SlideShow.scss";
+
+
+const SlideShow = ({auto}) => {
+	useEffect(() => {
+		if (auto) {
+			setInterval(nextSlide, interval);
+		}
+	}, [auto]);
+	const slides = document.querySelectorAll(".slide");
+	const interval = 5000;
+
+	
+
+	const [SlideShow] = useState([
+		{
+      id:1,
+			title: "Projectando um Futuro Melhor e saudavel",
+      subTitle: "Para Ti e a Sua Familia",
+      clss:"current"
+		},
+		{
+      id:2,
+			title: "Projectos Residências",
+			subTitle: "	Segurança & Bem-Estar"
+		},
+		{
+      id:3,
+			title: "Design Interno",
+			subTitle: "Para Ti e a Sua Familia"
+		},
+		{
+      id:4,
+			title: "Projectos Comerciais",
+			subTitle: "Lazer & Bem-Estar"
+		}
+  ]);
+
+	return (
+		
+		<Fragment>
+			<div className="slider">
+				{SlideShow.map((slide) => (
+					<div className={`slide ${slide.clss}`} key={slide.id}>
+						<div className= {`content${slide.id}`}>
+							<h3 className="light grey-text text-lighten-3 slide-text">
+								{slide.title}
+							</h3>
+							<h5 className="light grey-text text-lighten-3 slide-text">
+								{slide.subTitle}
+							</h5>
+						</div>
+					</div>
+				))}
+			</div>
+
+			<div className="button">
+				<button id="prev" onClick={prevSlide}>
+					<i className="fas fa-arrow-left"></i>
+				</button>
+
+				<button id="next" onClick={nextSlide}>
+					<i className="fas fa-arrow-right"></i>
+				</button>
+			</div>
+		</Fragment>
+	);
+};
+
+export default SlideShow;
